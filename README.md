d6516ffc03a9d0a44817f37786d1c07b065c74b9
Efe <154589585+javertus@users.noreply.github.com>
2025-09-06 15:18:38 +0300

    Refactor song state and add now playing updates
    Refactored the song_create class to use more descriptive and consistent attribute names, and added new attributes for message tracking and concurrency control. Introduced periodic now playing embed updates with progress bars, and improved handling of song status changes (e.g., skipped, finished, paused, bot left). Enhanced user feedback and embed visuals throughout the bot's commands.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lavalink-discord-bot
 Basic music bot for discord that uses lavalink youtube
+
+# Known Issues
+
+- Song position does not keeps up with filters applied to song
+- Cannot retrieve exact song position on bot kick event because voice client already killed
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
-import math
-from discord import app_commands
 import wavelink
+import math
+import asyncio
+from discord import app_commands
 from typing import Optional, cast
 
 client = discord.Client(intents=discord.Intents.all())
@@ -13,31 +14,56 @@ LAVALINK_PASSWORD = "youshallnotpass"
 
 class song_create:
     def __init__(self, interaction: discord.Interaction, track: wavelink.Playable):
-        self.embed = create_embed(interaction, track)
-        self.track = track
-        self.url = track.uri
-        self.interaction = interaction
-        self.channel = interaction.channel_id
-        self.requester = interaction.user
-        self.id = interaction.guild.id
-        self.voice_client = interaction.guild.voice_client
-        self.isPlaying = False
-        self.isLoop = False
-        self.isPaused = False
+        self.embed: discord.Embed = create_embed(interaction, track)
+        self.track: wavelink.Playable = track
+        self.url: str = track.uri
+        self.interaction: discord.Interaction = interaction
+        self.channel: int = interaction.channel_id
+        self.requester: discord.User = interaction.user
+        self.id: int = interaction.guild.id
+        self.voice_client: wavelink.Player = interaction.guild.voice_client
+        self.is_loop = False
+        self.is_paused = False
         self.skip = False
-        self.skipValue = 0
-        self.skipVoters = []
+        self.skip_value = 0
+        self.position: int = 0
+        self.skip_voters: dict = []
+        self.message_id: int = None
+        self.lock: asyncio.Lock = asyncio.Lock()
 
-def create_embed(interaction: discord.Interaction, track: wavelink.Playable):
-    embed = (discord.Embed(
-        description=f'```css\n{track.title}\n```',
-        color=discord.Color.blurple())
-        .add_field(name='Duration', value=f'{track.length // 60000}:{(track.length // 1000) % 60:02}')
-        .add_field(name='Requested by', value=interaction.user.mention)
-        .add_field(name='Uploader', value=f'{track.author}')
-        .add_field(name='URL', value=f'[Click]({track.uri})'))
-    if track.artwork:
-        embed.set_thumbnail(url=track.artwork)
+def create_embed(interaction: discord.Interaction, track: wavelink.Playable,position: int = 0, show_progress: bool = False, title: str = "Now Playing", color: discord.Color = discord.Color.blurple()):
+    total_seconds = track.length // 1000
+    current_seconds = position // 1000
+    remaining_seconds = total_seconds - current_seconds
+
+    embed = discord.Embed(
+        description = f'```css\n{track.title}\n```',
+        title = title,
+        color = color
+    )
+
+    if show_progress:
+        bar_length = 30
+        filled_length = int(bar_length * current_seconds / total_seconds)
+        bar = ("**" if filled_length else "") + "-" * filled_length + ("**" if filled_length else "") + "🔘" + "-" * (bar_length - filled_length)
+
+        embed.add_field(
+            name='Duration',
+            value=(
+                f'{current_seconds // 60}:{current_seconds % 60:02} / '
+                f'{total_seconds // 60}:{total_seconds % 60:02} '
+                f'(`{remaining_seconds // 60}:{remaining_seconds % 60:02}` left)'
+            )
+        )
+        embed.add_field(name='Progress', value=bar, inline=False)
+    else:
+        embed.add_field(name='Duration', value=f'{total_seconds // 60}:{total_seconds % 60:02}')
+
+    embed.add_field(name='Requested by', value=interaction.user.mention)
+    embed.add_field(name='Uploader', value=f'{track.author}')
+    embed.add_field(name='URL', value=f'[Click]({track.uri})')
+    if track.artwork: embed.set_thumbnail(url=track.artwork)
+
     return embed
 
 def check_voice_state(interaction: discord.Interaction):
@@ -56,6 +82,55 @@ async def ensure_player(interaction: discord.Interaction):
         player.autoplay = wavelink.AutoPlayMode.disabled
     return player
 
+async def update_now_playing_status(song: song_create, status: str, position: int = 0, color: discord.Color = discord.Color.blurple()):
+    async with song.lock:
+        try:
+            channel = client.get_channel(song.channel)
+            if not channel:
+                return
+            msg = await channel.fetch_message(song.message_id)
+            embed = create_embed(song.interaction, song.track, position, True, status, color)
+            await msg.edit(embed=embed)
+        except Exception as e:
+            print(f"Failed to update status: {e}")
+
+async def edit_message_by_id(channel, message_id, new_embed):
+    try:
+        message = await channel.fetch_message(message_id)
+        await message.edit(embed=new_embed)
+    except discord.NotFound:
+        print(f"Message {message_id} not found")
+    except discord.Forbidden:
+        print("No permission to edit message")
+
+async def update_now_playing(guild_id: int):
+    song: song_create = guilds[guild_id][0]
+    while True:
+        async with song.lock:
+            if guild_id not in guilds:
+                break
+            if song.message_id != guilds[guild_id][0].message_id:
+                song = guilds[guild_id][0]
+                continue
+
+            player: wavelink.Player = song.voice_client
+            song.position = player.position
+
+            title = "Now Playing"
+            if song.is_loop: title += " (Loop)"
+            if song.is_paused: title += " (Paused)"
+
+            song.embed = create_embed(song.interaction, song.track, song.position, True, title)
+            try:
+                channel = client.get_channel(song.channel)
+                if channel:
+                    msg = await channel.fetch_message(song.message_id)
+                    await msg.edit(embed=song.embed)
+            except Exception as e:
+                print(f"Update embed failed: {e}")
+
+            await asyncio.sleep(5)
+
 @client.event
 async def on_ready():
     await tree.sync()
@@ -72,6 +147,8 @@ async def on_voice_state_update(member: discord.Member, before: discord.VoiceSta
     if not guilds.get(member.guild.id): return # to check if bot just disconnected after player's queue ended
     if member.id != client.user.id: return
     if after.channel is None:
+        song: song_create = guilds[member.guild.id][0]
+        client.loop.create_task(update_now_playing_status(song, "Bot Kicked", song.position, discord.Color.red()))
         guilds.pop(member.guild.id)
 
 @client.event
@@ -79,21 +156,26 @@ async def on_wavelink_track_end(payload: wavelink.TrackEndEventPayload):
     player = payload.player
     if not player: return
     guild_id = player.guild.id
-
+    song: song_create = guilds[guild_id][0]
+    
     #filters: wavelink.Filters = player.filters
     #filters.timescale.set(pitch=1, speed=1, rate=1)
     #await player.set_filters(filters)
-    
-    if len(guilds[guild_id]) == 1 and not guilds[guild_id][0].isLoop:
-        await player.disconnect()
-        guilds.pop(guild_id)
-        return
 
-    if not guilds[guild_id][0].isLoop or guilds[guild_id][0].skip: 
+    if payload.reason == "finished":
+        client.loop.create_task(update_now_playing_status(song, "Finished playing", song.track.length, discord.Color.green()))
+
+    if not song.is_loop or song.skip:
+        if len(guilds[guild_id]) == 1:
+            guilds.pop(guild_id)
+            await player.disconnect()
+            return
         guilds[guild_id].pop(0)
-        guilds[guild_id][0].embed.title = "Now Playing"
-        await client.get_channel(guilds[guild_id][0].channel).send(embed=guilds[guild_id][0].embed, silent=True)
-    await player.play(guilds[guild_id][0].track)
+        song: song_create = guilds[guild_id][0]
+        song.embed = create_embed(song.interaction, song.track, 0, show_progress=True)
+        msg = await client.get_channel(song.channel).send(embed=song.embed, silent=True)
+        song.message_id = msg.id
+    await player.play(song.track)
 
 
 @client.event
@@ -104,28 +186,32 @@ async def setup_hook():
 @tree.command(name='play', description="Plays a song. URL or search text required")
 async def play(interaction: discord.Interaction, search: str):
     try:
-        await interaction.response.defer(ephemeral=False, thinking=True)
         check_voice_state(interaction)
 
+        await interaction.response.defer(ephemeral=False, thinking=True)
         tracks = await wavelink.Playable.search(search)
         if not tracks:
             await interaction.followup.send(f"No match found for **{search}**.")
             return
         track = tracks[0]
-        new_song = song_create(interaction, track)
 
         player = await ensure_player(interaction)
+        new_song = song_create(interaction, track)
 
         guild_id = interaction.guild.id
         guilds[guild_id].append(new_song) if guilds.get(guild_id) else guilds.update({guild_id: [new_song]})
 
         if len(guilds[guild_id]) == 1:
             await player.play(track)
-            guilds[guild_id][0].embed.title = "Now Playing"
-            await interaction.followup.send(embed=guilds[guild_id][0].embed)
+            song: song_create = guilds[guild_id][0]
+            song.embed = create_embed(interaction, track, 0, show_progress=True)
+            msg = await interaction.followup.send(embed=song.embed)
+            song.message_id = msg.id
+            client.loop.create_task(update_now_playing(guild_id))
         else:
             song_index = len(guilds[guild_id]) - 1
             guilds[guild_id][song_index].embed.title = "Added To Queue"
+            guilds[guild_id][song_index].embed.color = discord.Color.greyple()
             await interaction.followup.send(embed=guilds[guild_id][song_index].embed)
     except Exception as e:
         await interaction.followup.send(e)
@@ -134,11 +220,12 @@ async def play(interaction: discord.Interaction, search: str):
 async def loop(interaction: discord.Interaction):
     try:
         check_voice_state(interaction)
-        if guilds[interaction.guild.id][0].isLoop:
-            guilds[interaction.guild.id][0].isLoop = False
+        song: song_create = guilds[interaction.guild.id][0]
+        if song.is_loop:
+            song.is_loop = False
             await interaction.response.send_message("Song loop removed.")
         else:
-            guilds[interaction.guild.id][0].isLoop = True
+            song.is_loop = True
             await interaction.response.send_message("Song loop enabled.")
     except Exception as e:
         await interaction.response.send_message(e)
@@ -147,18 +234,20 @@ async def loop(interaction: discord.Interaction):
 async def skip(interaction: discord.Interaction):
     try:
         check_voice_state(interaction)
+        song: song_create = guilds[interaction.guild.id][0]
         player: wavelink.Player = interaction.guild.voice_client
-        if interaction.user in guilds[interaction.guild.id][0].skipVoters:
+        if interaction.user in song.skip_voters:
             raise app_commands.AppCommandError("You have already voted to skip this song.")
-        guilds[interaction.guild.id][0].skipVoters.append(interaction.user)
-        guilds[interaction.guild.id][0].skipValue += 1
+        song.skip_voters.append(interaction.user)
+        song.skip_value += 1
         user_count = math.ceil(len(interaction.user.voice.channel.members) / 2)
-        if guilds[interaction.guild.id][0].skipValue >= user_count or guilds[interaction.guild.id][0].requester == interaction.user:
-            guilds[interaction.guild.id][0].skip = True
-            await player.skip(force=True)
+        if song.skip_value >= user_count or song.requester == interaction.user:
+            song.skip = True
+            client.loop.create_task(update_now_playing_status(song, "Skipped", song.voice_client.position, discord.Color.yellow()))
             await interaction.response.send_message("Song skipped.")
+            await player.skip(force=True)
         else:
-            await interaction.response.send_message(f"Voted to skip the song. {guilds[interaction.guild.id][0].skipValue}/{user_count} votes.")
+            await interaction.response.send_message(f"Voted to skip the song. {song.skip_value}/{user_count} votes.")
     except Exception as e:
         await interaction.response.send_message(e)
 
@@ -183,9 +272,10 @@ async def queue(interaction: discord.Interaction, page: int = 1):
 async def pause(interaction: discord.Interaction):
     try:
         check_voice_state(interaction)
+        song: song_create = guilds[interaction.guild.id][0]
         player: wavelink.Player = interaction.guild.voice_client
         await player.pause(True)
-        guilds[interaction.guild.id][0].isPaused = True
+        song.is_paused = True
         await interaction.response.send_message("Song paused.")
     except Exception as e:
         await interaction.response.send_message(e)
@@ -194,9 +284,10 @@ async def pause(interaction: discord.Interaction):
 async def resume(interaction: discord.Interaction):
     try:
         check_voice_state(interaction)
+        song: song_create = guilds[interaction.guild.id][0]
         player: wavelink.Player = interaction.guild.voice_client
         await player.pause(False)
-        guilds[interaction.guild.id][0].isPaused = False
+        song.is_paused = False
         await interaction.response.send_message("Song resumed.")
     except Exception as e:
         await interaction.response.send_message(e)
@@ -205,9 +296,11 @@ async def resume(interaction: discord.Interaction):
 async def leave(interaction: discord.Interaction):
     try:
         check_voice_state(interaction)
+        song: song_create = guilds[interaction.guild.id][0]
         player: wavelink.Player = interaction.guild.voice_client
         await player.disconnect()
-        guilds.pop(interaction.guild.id, None)
+        client.loop.create_task(update_now_playing_status(song, "Bot Leaved", song.voice_client.position, discord.Color.orange()))
+        guilds.pop(interaction.guild.id)
         await interaction.response.send_message("Disconnected.")
     except Exception as e:
         await interaction.response.send_message(e)
@@ -237,7 +330,7 @@ async def timescale(
             rate = rate if rate else (filters.timescale.payload.get("rate") or 1.0)
         )
 
-        await player.set_filters(filters)
+        await player.set_filters(filters, seek=True)
 
         await interaction.response.send_message(
             f'Changed timescale.\n'
